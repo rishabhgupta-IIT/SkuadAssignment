6d554a243f2971cb56c819f0e65aca5f8a30c6c9
Rishabh Gupta <rishabh.gupta@care.com>
2021-02-20 13:06:37 +0530

    Initial skeleton of the project

---
+++ SkuadAssignment/SkuadAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkuadAssignment
+//
+//  Created by Rishabh Gupta on 2021-02-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
