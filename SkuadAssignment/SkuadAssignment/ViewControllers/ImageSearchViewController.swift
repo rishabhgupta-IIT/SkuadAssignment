d3aafa8a1b448fb0a46355773929d12aead2cf2e
Rishabh Gupta <rishabh.gupta@care.com>
2021-02-21 11:28:21 +0530

    trying pagination in different way

--- SkuadAssignment/SkuadAssignment/ViewControllers/ImageSearchViewController.swift
+++ SkuadAssignment/SkuadAssignment/ViewControllers/ImageSearchViewController.swift
@@ -12,10 +12,12 @@ class ImageSearchViewController: UIViewController {
     @IBOutlet weak private var collectionView: UICollectionView!
     
     let searchController = UISearchController(searchResultsController: nil)
-    var currentPage = 1
-    var latestScrolledIndex = 0
+    var currentPage = 0
+//    var latestScrolledIndex = 0
+    var isLoadingList : Bool = false
     var listItems = [SearchImageItem]()
     var filterContentForSearchText: (() -> Void)?
+    var imageCache = [String: UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +29,7 @@ class ImageSearchViewController: UIViewController {
             self.listItems.removeAll()
             self.prepareDataSource(self.searchController.searchBar.text!, self.currentPage)
         })
-        prepareDataSource("Delhi", currentPage)
+//        prepareDataSource("Flowers", currentPage)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -62,6 +64,7 @@ class ImageSearchViewController: UIViewController {
                     print("Received \(itemList.count)")
                     strongSelf.listItems += itemList
                     DispatchQueue.main.async {
+                        strongSelf.isLoadingList = false
                         strongSelf.collectionView.reloadData()
                     }
                 }
@@ -83,36 +86,32 @@ extension ImageSearchViewController: UICollectionViewDataSource, UICollectionVie
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if indexPath.row > (currentPage * NetworkManager.sharedInstance.perPage - 30) {
-            currentPage += 1
-            prepareDataSource(self.searchController.searchBar.text!, currentPage)
-        }
-        if indexPath.row > latestScrolledIndex {
-            latestScrolledIndex = indexPath.row
-        }
         let previewImageCell = collectionView.dequeueReusableCell(withReuseIdentifier: "previewImageCell", for: indexPath) as! PreviewImageCell
         let imageURL = listItems[indexPath.row].previewURL
-        if let previewImageData = listItems[indexPath.row].previewImageData {
+        if let imageAvailable = imageCache[imageURL] {
             print("Data already available for \(indexPath.row)")
-            previewImageCell.configure(UIImage(data: previewImageData))
+            previewImageCell.configure(imageAvailable)
         }
         else {
             print("fetching data for \(indexPath.row)")
             var previewImage = UIImage(named: "avatar-empty")
             previewImageCell.configure(previewImage)
-            if indexPath.row == latestScrolledIndex {
-                DispatchQueue.global().async { [weak self] in
-                    if let url = URL(string: imageURL),
-                       let data = try? Data(contentsOf: url) {
-                        print("fetched data for \(indexPath.row) with \(imageURL)")
-                        self?.listItems[indexPath.row].previewImageData = data
-                        previewImage = UIImage(data: data)
-                    }
-                    
-                    DispatchQueue.main.async {
-                        if let previewImageData = self?.listItems[indexPath.row].previewImageData {
-                            previewImageCell.configure(UIImage(data: previewImageData))
-                        }
+            DispatchQueue.global().async { [weak self] in
+                let index = indexPath
+                if let url = URL(string: imageURL),
+                   let data = try? Data(contentsOf: url) {
+                    print("fetched data for \(index.row) with \(imageURL)")
+//                    self?.listItems[index.item].previewImageData = data
+//                    previewImage = UIImage(data: data)
+                    self?.imageCache[imageURL] = UIImage(data: data)
+                }
+                
+                DispatchQueue.main.async {
+//                    if let previewImageData = self?.listItems[index.item].previewImageData {
+//                        previewImageCell.configure(UIImage(data: previewImageData))
+//                    }
+                    if let image = self?.imageCache[imageURL] {
+                        previewImageCell.configure(image)
                     }
                 }
             }
@@ -123,6 +122,25 @@ extension ImageSearchViewController: UICollectionViewDataSource, UICollectionVie
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let fullScreenVC = FullScreenImageViewController.viewController(listItems)
         navigationController?.pushViewController(fullScreenVC, animated: true)
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+//        if indexPath.item > (currentPage * NetworkManager.sharedInstance.perPage - 10) {
+//            currentPage += 1
+//            prepareDataSource("Flowers", currentPage)
+//        }
+//    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (((scrollView.contentOffset.y + scrollView.frame.size.height) > scrollView.contentSize.height ) && !isLoadingList) {
+            self.isLoadingList = true
+            self.loadMoreItemsForList()
+        }
+    }
+    
+    func loadMoreItemsForList() {
+        currentPage += 1
+        prepareDataSource("Flowers", currentPage)
     }
 }
 
